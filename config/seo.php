0e6f6ead52e8e2e92ce213afcfcebe36cb923368
System Administrator <root@BenjamindeMacBook-Pro.local>
2016-07-08 17:16:08 +0800

    First blog on Github

--- config/seo.php
+++ config/seo.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2014 OA Wu Design
+ * @author      Ben <benjamin0901@gmail.com>
+ * @copyright   Copyright (c) 2016 Ben
  */
 
 /* 網站的 seo 設定 */
 
   /* 主要的標題 */
-  $_title       = "OA's blog";
+  $_title       = "Ben's blog";
 
   /* 網址 */
   $_url         = $_domain . $_git_name;
 
   /* 所屬者 */
-  $_author      = '吳政賢(OA Wu)';
+  $_author      = '陳奎銘(Ben)';
 
   /* 主要關鍵字 */
-  $_keywords    = '吳政賢 | OA Wu | comdan66 | blog | php | javascript | jQuery | html | html | css | java';
+  $_keywords    = '陳奎銘 | Ben | R';
 
   /* 主要敘述文字 */
-  $_description = '紀錄生活、工作，這是一個屬於 OA 的個人部落格，內容皆屬於靜態 html 公開，版面也是自己實作切板，空間則是使用 github.io，如果對這 blog 技術有興趣的，非常歡迎到我 GitHub 參考囉！';
+  $_description = '紀錄學習歷程';
 
   /* Open Graph 相關設定 */
   $_og = array (
